14f683b8b7281ee79dcd0e4c405117f806a0cef5
emrebdr <emrebdr29@gmail.com>
2023-03-06 15:40:23 +0300

    add: more information to profile page about user

--- ui/src/ui-component/cards/ProfileCard.js
+++ ui/src/ui-component/cards/ProfileCard.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Rowing } from '@mui/icons-material';
 
-function OutlinedCard({ user }) {
+function OutlinedCard({ user, outOfOfficeDayList, userScheduleList }) {
+  console.log(userScheduleList)
   return (
       <Box display="flex" flexWrap="wrap">
         <React.Fragment>
@@ -28,7 +28,7 @@ function OutlinedCard({ user }) {
         </React.Fragment>
 
         <React.Fragment>
-          <CardContent sx={{ml: 20, width: 500, height: 160, backgroundColor: 'white'}}>
+          <CardContent sx={{ml: 15, width: 500, height: 160, backgroundColor: 'white'}}>
             <Typography style={{ fontSize: 15 }} variant="body2">
               <strong>Department Code:</strong> {user.departmentCode}
             </Typography>
@@ -43,6 +43,37 @@ function OutlinedCard({ user }) {
             </Typography>
             <Typography style={{ fontSize: 15 }} variant="body2">
               <strong>Zone Name:</strong> {user.zoneName}
+            </Typography>
+          </CardContent>
+        </React.Fragment>
+
+        <React.Fragment>
+          <CardContent sx={{mt: 5, width: 500, height: 160, backgroundColor: 'white'}}>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>Schedule Info</strong>
+            </Typography>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>Office Day Count:</strong> {userScheduleList.officeDay}
+            </Typography>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>WFH Day Count:</strong> {userScheduleList.workFromHome}
+            </Typography>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>Vacation Count:</strong> {userScheduleList.vacation}
+            </Typography>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>Total Day:</strong> {userScheduleList.totalDay}
+            </Typography>
+          </CardContent>
+        </React.Fragment>
+
+        <React.Fragment>
+          <CardContent sx={{mt: 5, ml: 15, width: 500, height: 160, backgroundColor: 'white'}}>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>Out Office Day Info</strong>
+            </Typography>
+            <Typography style={{ fontSize: 15 }} variant="body2">
+              <strong>Out Office Day Count:</strong> {outOfOfficeDayList.length}
             </Typography>
           </CardContent>
         </React.Fragment>
--- ui/src/views/user/profile/index.js
+++ ui/src/views/user/profile/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import ProfileCard from '../../../ui-component/cards/ProfileCard';
 import { useNavigate } from 'react-router-dom'; 
 import UserService from 'services/user/UserService';
+import OutOfOfficeDayService from 'services/out-of-office-day/OutOfOfficeDayService';
+import ScheduleService from 'services/schedule/ScheduleService';
 import ServiceCaller from 'services/ServiceCaller';
 import JWTUtil from 'utils/jwtUtil';
 import { toast } from 'react-toastify';
@@ -9,6 +11,20 @@ import { toast } from 'react-toastify';
 function Profile() {
   const navigate = useNavigate();
   const [user, setUser] = useState({});
+  const [outOfOfficeDayList, setOutOfOfficeDayList] = useState([]);
+  const [userScheduleList, setUserScheduleList] = useState([]);
+
+  const getMonthName = () => {
+    const date = new Date();
+    const month = date.toLocaleString('en-US', { month: 'long' });
+
+    return month;
+  }
+
+  const getYear = () => {
+    const date = new Date();
+    return date.getFullYear().toString();
+  }
 
   const getUser = () => {
     const serviceCaller = new ServiceCaller();
@@ -17,6 +33,50 @@ function Profile() {
     .then((res) => {
       if (res.status === 200) {
         setUser(res.data);
+        return
+      }
+
+      if (res.status === 401) {
+        toast.error("You are not authorized to access this page", { autoClose: 1000 });
+        this.navigate('/', { replace: true });
+        return
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const getUserScheduleList = () => {
+    const serviceCaller = new ServiceCaller();
+    ScheduleService.getActiveSchedule(serviceCaller, { 
+      userId: parseInt(sessionStorage.getItem('userId')),
+      dateMonth: getMonthName(),
+      dateYear: getYear()
+    })
+    .then((res) => {
+      if (res.status === 200) {
+        setUserScheduleList(res.data);
+        return
+      }
+
+      if (res.status === 401) {
+        toast.error("You are not authorized to access this page", { autoClose: 1000 });
+        this.navigate('/', { replace: true });
+        return
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const getOutOfOfficeDayList = () => {
+    const serviceCaller = new ServiceCaller();
+    OutOfOfficeDayService.getOutOfOfficeDays(serviceCaller, '')
+    .then((res) => {
+      if (res.status === 200) {
+        setOutOfOfficeDayList(res.data);
         return
       }
 
@@ -41,6 +101,8 @@ function Profile() {
       }
 
       getUser();
+      getOutOfOfficeDayList();
+      getUserScheduleList();
     })
     .catch((err) => {
       console.log(err);
@@ -50,7 +112,7 @@ function Profile() {
   return (
     <div>
       <h2>Profile</h2>
-      <ProfileCard user={user} />
+      <ProfileCard user={user} outOfOfficeDayList={outOfOfficeDayList} userScheduleList={userScheduleList}/>
       </div>
   )
 }
